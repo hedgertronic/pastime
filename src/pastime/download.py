98b6c8cd17adb1acf96e94f4aac1b7826fe36b41
Josh Hejka <josh@joshhejka.com>
2022-10-21 17:54:16 -0700

    Update download with exceptions from requests

--- src/pastime/download.py
+++ src/pastime/download.py
@@ -59,25 +59,29 @@ def _progress_group(current_app_progress, overall_progress) -> Group:
 
 def download_file(
     url: str,
-    output: io.StringIO,
     params: dict[str, list[str]],
     request_name: str | None = None,
     messages: Sequence[str] = None,
     **kwargs: Any,
 ) -> io.StringIO:
     return download_files(
-        url, output, [params], request_name=request_name, messages=messages, **kwargs
+        url,
+        [params],
+        request_name=request_name,
+        messages=messages,
+        **kwargs,
     )
 
 
 def download_files(
     url: str,
-    output: io.StringIO,
     params: Sequence[dict[str, list[str]]],
     request_name: str | None = None,
     messages: Sequence[str] = None,
     **kwargs: Sequence[Any],
 ) -> io.StringIO:
+    output = io.StringIO()
+
     if len(params) > 1:
         console.print(
             f"There are {len(params)} requests to make. This may take a while.",
@@ -116,10 +120,14 @@ def download_files(
             total=None,
         )
 
+        actual_length = 0
+
         with closing(
             requests.get(url=url, timeout=180, stream=True, **kwargs)
         ) as response:
-            total_length = int(response.headers["Content-Length"])
+            response.raise_for_status()
+
+            total_length = int(response.headers.get("Content-Length", 100_000))
 
             current_app_progress.update(task_id, total=total_length)
 
@@ -127,7 +135,13 @@ def download_files(
                 output.write(line + "\n")
                 current_app_progress.update(task_id, advance=len(line))
 
+                actual_length += len(line)
+
             current_app_progress.update(task_id, completed=total_length)
+
+        current_app_progress.update(
+            task_id, total=actual_length, completed=actual_length
+        )
 
         current_app_progress.stop_task(task_id)
         current_app_progress.update(task_id, description="[bold green]File downloaded!")
